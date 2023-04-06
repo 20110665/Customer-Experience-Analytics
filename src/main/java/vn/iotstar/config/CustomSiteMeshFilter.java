42c4050042a4d6bcb36f5c54090bd61d4a2c1a8d
20110665 <20110665@student.hcmute.edu.vn>
2023-04-06 22:38:38 +0700

    reInit

---
+++ src/main/java/vn/iotstar/config/CustomSiteMeshFilter.java
@@ -0,0 +1,16 @@
+package vn.iotstar.config;
+
+import org.sitemesh.builder.SiteMeshFilterBuilder;
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomSiteMeshFilter extends ConfigurableSiteMeshFilter{
+
+	@Override
+	protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
+		super.applyCustomConfiguration(builder);
+		builder.addDecoratorPath("/", "/decorators/home.jsp");
+	}
+	
+}
